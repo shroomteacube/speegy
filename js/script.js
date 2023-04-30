2f960e1047f7ed2dd0f1ab75553aef5c6242cda0
shroomteacube <shroomteacube@gmail.com>
2023-04-30 13:08:17 -0400

    added hitbox indicator and auto restart

--- js/script.js
+++ js/script.js
@@ -18,13 +18,17 @@ gameOver = false;
 
 projectiles = [];
 
-for(i=0;i<game.size;i++){
-    projectiles.push([-300 * i, Math.floor(Math.random() * 5)])
+function createGame(){
+    player.y = 2;
+    projectiles = [];
+    for(i=0;i<game.size;i++){
+        projectiles.push([-300 * i, Math.floor(Math.random() * 5)])
+    }
+    gameOver = false;
 }
 
 function setup(){
     createCanvas(1000, 500);
-    stroke(1);
 }
 
 function draw(){
@@ -32,28 +36,40 @@ function draw(){
         clear();
         image(pog, 200, player.y * 100, 100, 100);
 
+
         projectiles.forEach((e, i)=>{
             image(troll, 1000 - e[0], e[1] * 100, 100, 100);
             e[0]+=game.speed;
             if(e[1] == player.y && 1000 - e[0] < 300 && 1000 - e[0] > 200){
+                stroke(color("red"));
+                fill(255, 0, 0, 0);
+                rect(200, player.y * 100, 100, 100);
                 gameOver = true;
-                alert("YOU LOSE");
-                location.reload();
+                setTimeout(function(){
+                    createGame();
+                }, 1000);
             }
             
             if(i == projectiles.length-1 && 1000 - e[0] == -100){
-                alert("YOU WIN");
-                location.reload();
+                gameOver = true;
+
+                alert("YOU WIN (exit to play again)");
+                createGame();
             }
         });
-    } 
+    }
+    else{
+        console.log("hello world");
+    }
 }
 
 document.body.addEventListener("keydown", e=>{
-    if(e.key == "w" && player.y > 0){
+    if(e.key == "w" && player.y > 0 && gameOver == false){
         player.y--;
     }
-    if(e.key == "s" && player.y < 4){
+    if(e.key == "s" && player.y < 4 && gameOver == false){
         player.y++;
     }
-})
+});
+
+createGame();
